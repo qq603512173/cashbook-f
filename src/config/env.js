a768a5d1a77390ad1c747aacd65d0bef2cce400f
ozl <603512173@qq.com>
2021-09-27 11:33:31 +0800

    更新生产环境的数据库地址

--- src/config/env.js
+++ src/config/env.js
@@ -11,10 +11,12 @@ let routerMode = 'history'
 let baseImgPath = 'http://images.cangdu.org/'
 
 if (process.env.NODE_ENV === 'development') {
-  baseUrl = 'http://localhost:5000'
+  baseUrl = 'https://ouzilin.herokuapp.com'
+  //baseUrl = 'http://localhost:5000'
   //baseUrl = 'http://192.168.173.1:3000'
 } else {
-  baseUrl = 'https://ouzilin.herokuapp.com'
+  //baseUrl = 'https://ouzilin.herokuapp.com'
+  baseUrl = 'http://localhost:5000'
   //baseUrl = 'http://192.168.173.1:3000'
 }
 export {
